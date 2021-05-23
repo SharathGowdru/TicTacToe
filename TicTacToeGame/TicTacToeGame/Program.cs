9b3306a91a6f1e5b537ed376696662ef1cf3fe88
Sharath <Sharus9587@gmail.com>
2021-05-23 19:52:55 +0530

    Added : Creating empty TicTacToe board

---
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        public static void CreateEmptyBoard()
+        {
+            Char[] board = new char[10];
+            for(int i=0; i < 10 ; i++)
+            {
+                board[i] = ' ';
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe GAME in C#");
+            CreateEmptyBoard();
+        }
+    }
+}

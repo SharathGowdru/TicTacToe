5a4523ca6d6df8b317c04d956388b30caa97fd4a
Sharath <Sharus9587@gmail.com>
2021-05-23 20:51:11 +0530

    Added : Choose the letter that user want

--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -13,10 +13,37 @@ namespace TicTacToeGame
             }
 
         }
+        public static void ChooseLetter()
+        {
+            string computerChoice;
+            Console.WriteLine("Please enter your X/O : ");
+            string userChoice = Console.ReadLine();
+            while(true)
+            {
+                if( userChoice == "X" || userChoice == "x" )
+                {
+                    Console.WriteLine("computerChoice = O ");
+                    break;
+                }
+                else if(userChoice == "O" || userChoice == "o")
+                {
+                    Console.WriteLine("computerChoice = X ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, Please try again");
+                }
+
+
+            }
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe GAME in C#");
             CreateEmptyBoard();
+            ChooseLetter();
         }
     }
 }
